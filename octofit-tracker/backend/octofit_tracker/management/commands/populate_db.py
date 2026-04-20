12d576d316c9b407f6e6a2ae8b767e7a308a4cb9
moni-be <19931080+moni-be@users.noreply.github.com>
2026-04-20 17:12:42 +0000

    Add Django models, serializers, views, admin, tests, and API routing for OctoFit collections

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,39 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from octofit_tracker.models import Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Delete existing data
+        User = get_user_model()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        ironman = User.objects.create_user(username='ironman', email='ironman@marvel.com', password='password', team=marvel)
+        captain = User.objects.create_user(username='captainamerica', email='captain@marvel.com', password='password', team=marvel)
+        batman = User.objects.create_user(username='batman', email='batman@dc.com', password='password', team=dc)
+        superman = User.objects.create_user(username='superman', email='superman@dc.com', password='password', team=dc)
+
+        # Create activities
+        Activity.objects.create(user=ironman, type='run', duration=30)
+        Activity.objects.create(user=batman, type='cycle', duration=45)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        Workout.objects.create(name='Situps', description='Do 30 situps')
+
+        # Create leaderboard
+        Leaderboard.objects.create(user=ironman, score=100)
+        Leaderboard.objects.create(user=batman, score=90)
+
+        self.stdout.write(self.style.SUCCESS('Database populated with test data.'))
